97dbf6b3e8e905a03507947de3bb9b628b5dbe0e
tianxiao <tianxiao987@gmail.com>
2013-01-07 11:08:49 +0800

    stdaf.h position The w
    右击工程，选择properties -> configuration properties -> C/C++ -> commandline
     在additional options 打入 /wd443
    
    这样改就好了，为啥呢？
    因为在VC6中，如果没有显示的指定返回值类型，编译器将其视为默认整 形；但是vs2005不支持默认整形，解决这个问题不能修改每个没有显示指示返回值类型的函数地方，可以用wd4430来解决；具体的在工程选项中的c+ +/Command Line中添加/wd4430即可。

--- Montecarlo.cpp
+++ Montecarlo.cpp
@@ -2,9 +2,11 @@
 //
 
 #include <iostream>
-using namespace std;
+
 #include <math.h>
 #include "../gaps/pkgs/R3Graphics/R3Graphics.h"
+#include "../gaps/pkgs/R2Shapes/R2Image.h"
+#include "../gaps/pkgs/R3Graphics/R3Scene.h"
 #include "Vector.h"
 using namespace CDLib;
 #include <vector>
@@ -15,6 +17,7 @@ using std::string;
 // If I put the "stdafx.h" header file at the top
 // The R3Graphics.h could include all it's dependecy header files
 #include "stdafx.h"
+using namespace std;
 
 double globalstate = 0;
 double globali = 0;
@@ -27,11 +30,34 @@ static const int imageheight = imagewidth/4*3;
 static const int actualwidth = 512;
 static const int actualheight = 512;
 //static RNBoolean print_verbose = FALSE;
+static bool print_verbose = false;
+int grid_dimension = 3;
+int render_passes = 1;
+
+int linesrendered = 0;
+int linesdrawn = 0;
+
+#define CDVECTOR(x) (*(Vector3f*)&x)
+#define R3VECTOR(x) (*(R3Vector*)&x)
+
+float pixelsums[imagewidth][imageheight][4] = {0};
+float pixelvalues[actualwidth][actualheight][4] = {0};
+int pixelcounts[imagewidth][imageheight]={0};
+
+int win;
+
+bool changed = true;
+R2Image *image ;
+R3Scene *scene ;
+
+unsigned int texId = 0;
 
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	NoiseInit();
+
 	return 0;
 }
 
